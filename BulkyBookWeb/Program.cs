54e33b33668adc8be986e24e96414f557050d5a4
JeffCky <jeffcarterky@gmail.com>
2022-10-17 21:26:18 -0400

    First Commit

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddRazorPages();
+//builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
